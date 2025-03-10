37d1fb2333e88c944bad90add0aa7dc5843d8d82
Felipe <felipeduarte8266@gmail.com>
2025-03-10 00:31:07 -0300

    gg

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ const myObserver = new IntersectionObserver( (entries) => {
 const elements = document.querySelectorAll('.hidden')
 
 elements.forEach( (element) => myObserver.observe(element))
+
+const competenciasObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show-competencias");
+        } else {
+            entry.target.classList.remove("show-competencias");
+        }
+    });
+});
+
+const competenciasElements = document.querySelectorAll('.hidden-competencias');
+
+competenciasElements.forEach((element) => competenciasObserver.observe(element));
